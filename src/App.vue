cde23774752e77553f1cf953b4950353fab0ead6
ma-ya's create <hello.myscreate@gmail.com>
2020-09-08 21:06:41 +0900

    update vue cli 2 to 4

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,50 @@
   <div id="app">
     <menu-list></menu-list>
     <transition name="page">
-      <router-view/>
+      <router-view />
     </transition>
   </div>
 </template>
 
 <script>
-import 'normalize.css'
-import menuList from '@/components/menuList'
+import "normalize.css";
+import menuList from "@/components/menuList";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    menuList
-  }
-}
+    menuList,
+  },
+};
 </script>
 
 <style>
 * {
-  box-sizing : border-box;
+  box-sizing: border-box;
 }
 #app {
-  font-family: 'Heebo', 'Noto Sans JP', sans-serif;
+  font-family: "Heebo", "Noto Sans JP", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 a {
-  -webkit-tap-highlight-color : transparent;
+  -webkit-tap-highlight-color: transparent;
 }
 .page-enter-active {
-  position : relative;
+  position: relative;
   transition: all 0.5s ease-out;
 }
-.page-enter-to, .page-leave {
-  left : 0;
+.page-enter-to,
+.page-leave {
+  left: 0;
 }
-.page-enter, .page-leave-to {
+.page-enter,
+.page-leave-to {
   opacity: 0;
 }
 .page-enter {
-  left : -10px;
+  left: -10px;
 }
 .page-enter .mv {
-  left : 0px;
+  left: 0px;
 }
 </style>
---
+++ src/router/index.ts
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import top from "@/views/top.vue";
+import designs from "@/views/designs.vue";
+import cordings from "@/views/cordings.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "top",
+    component: top,
+  },
+  {
+    path: "/designs",
+    name: "designs",
+    component: designs,
+  },
+  {
+    path: "/cordings",
+    name: "cordings",
+    component: cordings,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+];
+
+const router = new VueRouter({
+  // mode: "history",
+  // base: "/wp-content/themes/mycreate/pages/wicd/",
+  // // base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
